fb74da71174fe0f1a8446634dc2e1c09471eb99c
cuc9 <cuc91111@gmail.com>
2017-01-06 21:39:59 +0200

    rest index + v1/movies works

--- src/main/java/com/movieland/MovielandController.java
+++ src/main/java/com/movieland/MovielandController.java
@@ -1,6 +1,9 @@
 package com.movieland;
 
+import com.movieland.dao.MovieDaoH2;
+import com.movieland.dbObjects.Movie;
 import org.omg.CORBA.Request;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 /**
  * Created by arpi on 05.01.2017.
@@ -19,12 +23,20 @@ import java.io.PrintWriter;
 @Controller
 @RequestMapping("/movieland")
 public class MovielandController {
-    @RequestMapping(path = "/", method = RequestMethod.GET)
+    @Autowired
+    private MovieDaoH2 movDao;
+
+    @RequestMapping(path = "/v1", method = RequestMethod.GET)
     public void index(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
         out.print("<h1>Hello Servlet</h1>");
         //req.getRequestDispatcher("/jsp/index.jsp").forward(req,resp);
     }
 
+    @RequestMapping(path = "v1/movies", method = RequestMethod.GET)
+    public void allMovies(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Movie> allMovies = movDao.getAll();
+        resp.getWriter().write(allMovies.toString());
+    }
 
 }
